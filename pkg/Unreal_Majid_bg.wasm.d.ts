3cb59fe3fcb7ec15bf8d5eb7dcc5c27fd7c8bf6b
Majid_Abdelilah <abdelilahmajid012@gmail.com>
2026-01-13 12:54:05 +0100

    feat: add WebAssembly support with initial type definitions and package configuration
    - Added Unreal_Majid_bg.wasm.d.ts for TypeScript definitions of WebAssembly functions.
    - Created package.json to define module metadata, including entry points and side effects.

---
+++ pkg/Unreal_Majid_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __wasm_bindgen_func_elem_1527: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_1359: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_1531: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_2499: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_2520: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_1525: (a: number, b: number, c: number, d: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
